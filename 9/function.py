73db725bd700b84818ac65537ebfedd56ef7b7fd
quangnvo <voquang.usth@gmail.com>
2023-12-12 21:22:22 +0800

    add the sum function

--- 9/function.py
+++ 9/function.py
@@ -3,3 +3,14 @@ def hello():
 
 
 hello()
+
+
+def sum(num1=0, num2=0):
+    if (type(num1) is not int or type(num2) is not int):
+        print("Error: Only numbers are allowed")
+        return
+    return num1 + num2
+
+
+total = sum(10, 20)
+print(total)
