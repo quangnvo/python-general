430327756642fab697648214273a5ee93e773595
quangnvo <voquang.usth@gmail.com>
2023-12-13 19:19:47 +0800

    remove the function hello world

--- 9/function.py
+++ 9/function.py
@@ -1,10 +1,3 @@
-def hello():
-    print("Hello World")
-
-
-hello()
-
-
 def sum(num1=0, num2=0):
     if (type(num1) is not int or type(num2) is not int):
         print("Error: Only numbers are allowed")
